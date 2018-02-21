4c045ad60edaa6c39544aa362f433d8c9cb45a14
Florian Spadaro <spadaro.florian@outlook.fr>
2018-02-21 17:49:36 +0100

    connexion utilisateur

--- API/functions.php
+++ API/functions.php
@@ -1,4 +1,18 @@
 <?php
+    function connexion($login, $mdp)
+    {
+        include("connexionBddErp.php");
 
+        $user = null;
 
+        $req = $bddErp->prepare("SELECT e.name_related, e.work_email FROM res_users u JOIN hr_employee e ON e.id = u.ag_employee_id WHERE login = ? AND password = ?");
+        $req->execute(array($login, $mdp));
+        if($data = $req->fetch())
+        {
+            $user = (object) array();
+            $user->name = $data["name_related"];
+            $user->mail = $data["work_email"];
+        }
+        return json_encode($user);
+    }
 ?>
--- index.php
+++ index.php
@@ -10,35 +10,49 @@
 
 	<body>
 	<div class="container">
-	<div class="row">
-			<div class="span12">
-				<fieldset id="login_field">
-					<div id="legend">
-						<legend class="login_label">Connexion<label class="login_label pull-right">SOBECA</label></legend>	
-					</div>
-					<div class="control-group">
-					<!-- Username -->
-						<label class="control-label login_label"  for="username" >Nom d'utilisateur</label>
-					<div class="controls">
-						<input type="text" id="username" name="username" placeholder="" class="input-xlarge form-control">
-					</div>
-					</div>
-					<div class="control-group">
-					<!-- Password-->
-						<label class="control-label login_label" for="password">Mot de passe</label>
-					<div class="controls">
-						<input type="password" id="password" name="password" placeholder="" class="input-xlarge form-control">
-					</div>
-					</div>
-					<div class="control-group">
-					<!-- Button -->
-					<div class="controls">
-						<button class="btn btn-primary pull-right" id="login_in">Se connecter</button>
-					</div>
-					</div>
-				</fieldset>
-			</div>
-		</div>
+		<?php
+			if(!isset($user) || $user == null)
+			{
+				?>
+				<div class="row">
+					<form id="formulaireConnexion" method="post" action="#">
+						<div class="span12">
+							<fieldset id="login_field">
+								<div id="legend">
+									<legend class="login_label">Connexion<label class="login_label pull-right">SOBECA</label></legend>	
+								</div>
+								<div class="control-group">
+								<!-- Username -->
+									<label class="control-label login_label"  for="username" >Nom d'utilisateur</label>
+								<div class="controls">
+									<input type="text" id="username" name="username" placeholder="" class="input-xlarge form-control">
+								</div>
+								</div>
+								<div class="control-group">
+								<!-- Password-->
+									<label class="control-label login_label" for="password">Mot de passe</label>
+								<div class="controls">
+									<input type="password" id="password" name="password" placeholder="" class="input-xlarge form-control">
+								</div>
+								</div>
+								<div class="control-group">
+								<!-- Button -->
+								<div class="controls">
+									<button type="submit" class="btn btn-primary pull-right" id="login_in">Se connecter</button>
+								</div>
+								</div>
+							</fieldset>
+						</div>
+						<input type="hidden" name="user_id" id="user_id" value="" />
+					</form>
+				</div>
+				<?php
+			}
+			else{
+				echo "BONJOUR ".$user->name;
+			}
+		?>
+	
 	</div>
         <?php include("footer.php"); ?>
 	</body>
